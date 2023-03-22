d6d8a094d082aa714b690ac254943ae17d9a5202
pnadolny13 <patnadolny@gmail.com>
2023-03-22 15:42:38 -0400

    format variants for gh

--- hub_utils/main.py
+++ hub_utils/main.py
@@ -124,17 +124,15 @@ def get_variant_names(
     # sdk_based: bool = typer.Option(False),
 ):
     select_subset = [
-        "extractors/tap-snowflake/meltanolabs.yml",
-        # "extractors/tap-github/meltanolabs.yml",
-        # "extractors/tap-cloudwatch/meltanolabs.yml"
+        "extractors/tap-cloudwatch/meltanolabs.yml",
     ]
     files = []
     for yaml_file in find_all_yamls(f_path=f"{hub_root}/_data/meltano/"):
         suffix = yaml_file.replace(f"{hub_root}/_data/meltano/", "")
         if suffix in select_subset:
             files.append(yaml_file)
-    # print(json.dumps(files))
-    print(",".join(files))
+    formatted_output = [{"source-name": "/".join(file.split("/")[-3:])} for file in files]
+    print(json.dumps(formatted_output))
 
 @app.command()
 def extract_metadata_v2(
